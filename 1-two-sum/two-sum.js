98805ed33041dff1e3d50496e1bc6fcf8efa602f
Halil Can Uyanık <101755107+halilcanuyanik@users.noreply.github.com>
2025-10-02 03:15:44 +0300

    Update two-sum.js

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,8 +6,7 @@
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
-            if (i !== j) {
-                if (nums[i] + nums[j] === target) {
+            if ((i !== j) && (nums[j] === (target - nums[i]))) {
                     return [i, j];
                 }
             }
